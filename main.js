4e7720916269cacd2a4dc24b393a8d3604216e03
Martinius <mar109us@gmail.com>
2026-04-25 01:13:52 +0200

    major cleanup and learning about common coding conventions/standardized code

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-const buttonPassword = document.getElementById("button-password")
+const id = (calledId) => document.getElementById(calledId);
+const showCss = (element) => (element.style.display = "block");
+const hideCss = (element) => (element.style.display = "none");
 
-const page = {
-    default: document.getElementById("page-default"),
-    password: document.getElementById("page-password"),
+const elements = {
+	pages: {
+		default: id("page-default"),
+		password: id("page-password"),
+	},
+	buttons: {
+		password: id("button-password"),
+	},
+};
+
+const states = {
+	show(pageName) {
+		const pageToChange = elements.pages[pageName];
+
+		if (pageToChange) {
+			showCss(pageToChange);
+		}
+	},
+
+	hide(pageName) {
+		const pageToChange = elements.pages[pageName];
+
+		if (pageToChange) {
+			hideCss(pageToChange);
+		}
+	},
+};
+
+elements.buttons.password.addEventListener("click", pagePasswordShow);
+function pagePasswordShow() {
+	states.hide("default");
+	states.show("password");
 }
-
-const state = {
-    show(calledPage) {
-        const pageToChange = page[calledPage]
-
-        if (pageToChange) {
-            pageToChange.style.display = "block"
-        }
-    },
-
-    hide(calledPage) {
-        const pageToChange = page[calledPage]
-
-        if (pageToChange) {
-            pageToChange.style.display = "none"
-        }
-    },
-}
-
-buttonPassword.addEventListener("click", pagePasswordShow)
-    function pagePasswordShow() {
-        state.hide("default")
-        state.show("password")
-    }
